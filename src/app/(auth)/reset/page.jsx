e7a63f145563d813c2ec6fb78cc30f3986e9ceba
전민기 <mingi@jeonmingiui-MacBookAir.local>
2023-08-06 18:37:14 +0900

    [Feat]:비밀번호 수정페이지

--- src/app/(auth)/reset/page.jsx
+++ src/app/(auth)/reset/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ResetClient from "./ResetClient";
 
 const Reset = () => {
-  return <div>ResetPage</div>;
+  return <ResetClient />;
 };
 
-export default ResetPage;
+export default Reset;
