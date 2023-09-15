adee43b8e9243e3c9700e721d1080fe8a8dc3fd8
podoco <mingi@jeonmingiui-MacBookAir.local>
2023-09-15 16:31:37 +0900

    Product데이터 리덕스 스토어에 저장하기

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,17 +1,46 @@
 "use client";
 import useFetchCollection from "../../hooks/useFetchCollection";
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Product.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  GET_PRICE_RANGE,
+  STORE_PRODUCTS,
+} from "../../redux/slice/productSlice";
+import Loader from "../loader/Loader";
+import ProductList from "./productList/ProductList";
+import ProductFilter from "./productFilter/ProductFilter";
 
 const Product = () => {
-  //const { data, isLoading } = useFetchCollection("products");
+  const { data, isLoading } = useFetchCollection("products");
 
-  console.log(data);
+  //store에 저장
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    );
+
+    dispatch(
+      GET_PRICE_RANGE({
+        products: data,
+      })
+    );
+  }, [data, dispatch]);
+
+  //store에서 가져오기
+  const products = useSelector(selectProducts);
 
   return (
     <section className={styles.product}>
-      <aside className={styles.filter}></aside>
-      <div className={styles.content}></div>
+      <aside className={styles.filter}>
+        {isLoading ? null : <ProductFilter />}
+      </aside>
+      <div className={styles.content}>
+        {isLoading ? <Loader basic /> : <ProductList />}
+      </div>
     </section>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import authReducer from './slice/authSlice'
+import Product from '../components/product/Product';
+import productReducer from './slice/productSlice'
 
 const rootReducer = combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  product: productReducer
 })
 
 const store = configureStore({
